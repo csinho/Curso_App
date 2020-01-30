218b0c5c789b386334379858ec3fea5d7ac76666
csinho <csinho.01@hotmail.com>
2020-01-29 23:10:51 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={ styles.cont }>Banana!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cont: {
+    color: '#000',
+    fontSize: 90,
+    fontWeight: 'bold'
+  }
+});
